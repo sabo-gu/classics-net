f63ca01f71b4144855e6886780cebd37e5c2bf5d
顾炳斌（萨博） <sabo.gbb@xuelangyun.com>
2021-11-10 11:37:36 +0800

    'a'

--- LeNet/LeNet.py
+++ LeNet/LeNet.py
@@ -6,7 +6,7 @@ class LeNet(nn.Module):
 
     def __init__(self):
         super(LeNet,self).__init__()
-        self.conv1=nn.Conv2d(1,6,5)
+        self.conv1=nn.Conv2d(1,6,5,padding=2)
         self.conv2=nn.Conv2d(6,16,5)
         self.fc1=nn.Linear(16*5*5,120)
         self.fc2=nn.Linear(120,84)
@@ -17,7 +17,7 @@ class LeNet(nn.Module):
         x=F.max_pool2d(x,(2,2))
         x=F.relu(self.conv2(x))
         x=F.max_pool2d(x,2)
-        x=torch.flatten(x,1)
+        x = x.view(x.size(0), -1)
         x=F.relu(self.fc1(x))
         x=F.relu(self.fc2(x))
         x=self.fc3(x)
---
+++ LeNet/train.py
@@ -0,0 +1,69 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from LeNet import LeNet
+
+train_data=datasets.MNIST(root='data',train=True,download=True,transform=transforms.ToTensor())
+test_data=datasets.MNIST(root='data',train=False,download=True,transform=transforms.ToTensor())
+batch_size=64
+train_dataloader=DataLoader(train_data,batch_size=batch_size,shuffle=True)
+test_dataloader=DataLoader(test_data,batch_size=batch_size,shuffle=True)
+
+device="cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+model=LeNet().to(device)
+print(model)
+
+loss_fn=nn.CrossEntropyLoss()
+optimizer=torch.optim.SGD(model.parameters(),lr=1e-3)
+
+
+def train(dataloader,model,loss_fn,optimizer):
+    size=len(dataloader.dataset)
+    model.train()
+    for batch,(X,y) in enumerate(dataloader):
+        X,y =X.to(device),y.to(device)
+
+        pred=model(X)
+        loss=loss_fn(pred,y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100==0:
+            loss ,current =loss.item(),batch*len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+
+
+
+
+
+
+def _test(dataloader,model,loss_fn):
+    size=len(dataloader.dataset)
+    num_batches=len(dataloader)
+    model.eval()
+    test_loss,correct=0,0
+    with torch.no_grad():
+        for X,y in dataloader:
+            X,y=X.to(device),y.to(device)
+            pred=model(X)
+            test_loss+=loss_fn(pred,y).item()
+            correct+=(pred.argmax(1)==y).type(torch.float).sum().item()
+        test_loss/=num_batches
+        correct/=size
+        print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+
+epoch=50
+for t in range(epoch):
+    print(f"Epoch {t + 1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    _test(test_dataloader, model, loss_fn)
